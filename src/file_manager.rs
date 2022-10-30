2bd13b0fba276aa6912bfc5a7221cc4c28967df0
Nic McPhee <mcphee@morris.umn.edu>
2022-10-29 21:42:42 -0500

    Start writing `FileManager` tests
    This does some simple initial tests. Not a ton, but it's a start.

--- src/file_manager.rs
+++ src/file_manager.rs
@@ -31,3 +31,80 @@ impl FileManager {
         todo!()
     }
 }
+
+#[cfg(test)]
+mod process_packet_tests {
+    use crate::{packets::{Header, Packet, Data}, file_manager::FileManager};
+
+    #[test]
+    fn just_header_packet() {
+        let test_file_name = "test_file.txt".to_string();
+        let header = Header {
+            file_id: 37,
+            file_name: test_file_name.clone(),
+        };
+
+        let mut file_manager = FileManager::default();
+        file_manager.process_packet(Packet::Header(header));
+
+        let map = file_manager.map;
+        assert_eq!(map.len(), 1);
+        let file_number = map.keys().nth(0).unwrap();
+        let packet_group = map.get(file_number).unwrap();
+        assert_eq!(test_file_name, packet_group.file_name.clone().unwrap());
+        assert_eq!(None, packet_group.expected_number_of_packets);
+        assert_eq!(0, packet_group.packets.len());
+    }
+
+    #[test]
+    fn just_data_packet_not_last() {
+        let file_id = 37;
+        let packet_number = 82;
+        let is_last_packet = false;
+        let bytes: Vec<u8> = vec![5, 8, 9];
+        let data_packet = Data {
+            file_id,
+            packet_number,
+            is_last_packet,
+            data: bytes.clone(),
+        };
+
+        let mut file_manager = FileManager::default();
+        file_manager.process_packet(Packet::Data(data_packet));
+
+        let map = file_manager.map;
+        assert_eq!(map.len(), 1);
+        let file_number = map.keys().nth(0).unwrap();
+        let packet_group = map.get(file_number).unwrap();
+        assert_eq!(None, packet_group.file_name);
+        assert_eq!(None, packet_group.expected_number_of_packets);
+        assert_eq!(1, packet_group.packets.len());
+        assert_eq!(bytes, packet_group.packets[&packet_number]);
+    }
+
+    #[test]
+    fn just_data_packet_is_last() {
+        let file_id = 37;
+        let packet_number = 82;
+        let is_last_packet = true;
+        let bytes: Vec<u8> = vec![5, 8, 9];
+        let data_packet = Data {
+            file_id,
+            packet_number,
+            is_last_packet,
+            data: bytes.clone(),
+        };
+
+        let mut file_manager = FileManager::default();
+        file_manager.process_packet(Packet::Data(data_packet));
+
+        let map = file_manager.map;
+        assert_eq!(map.len(), 1);
+        let file_number = map.keys().nth(0).unwrap();
+        let packet_group = map.get(file_number).unwrap();
+        assert_eq!(None, packet_group.file_name);
+        assert_eq!(Some(1 + packet_number as usize), packet_group.expected_number_of_packets);
+        assert_eq!(1, packet_group.packets.len());
+        assert_eq!(bytes, packet_group.packets[&packet_number]);
+    }
+}
