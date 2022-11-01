de1b5cf54074ad5dc5d4edc6bcd0352a04a8fb98
Nic McPhee <mcphee@morris.umn.edu>
2022-10-31 22:27:58 -0500

    Add quickcheck and full packet set tests
    This adds quickcheck tests for handling single packets, and a "normal" test for checking a full packet set.
    
    I really should add one more set of quickcheck tests that deal with handling multiple packets from different files.

--- src/file_manager.rs
+++ src/file_manager.rs
@@ -108,3 +108,95 @@ mod process_packet_tests {
         assert_eq!(bytes, packet_group.packets[&packet_number]);
     }
 }
+
+#[cfg(test)]
+mod quickcheck_tests {
+    use std::ops::Deref;
+
+    use crate::packets::{Header, Data};
+
+    use super::*;
+
+    #[quickcheck_macros::quickcheck]
+    fn header_doesnt_crash(packet: Header) -> bool {
+        let mut file_manager = FileManager::default();
+        file_manager.process_packet(Packet::Header(packet));
+        true
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn data_doesnt_crash(packet: Data) -> bool {
+        let mut file_manager = FileManager::default();
+        file_manager.process_packet(Packet::Data(packet));
+        true
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn header_sets_name(packet: Header) -> bool {
+        let mut file_manager = FileManager::default();
+        assert_eq!(None, file_manager.map.get(&packet.file_id));
+        file_manager.process_packet(Packet::Header(packet.clone()));
+        assert_eq!(0, file_manager.map.get(&packet.file_id).unwrap().packets.len());
+        file_manager.map.get(&packet.file_id).unwrap().file_name == Some(packet.file_name.clone())
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn data_add_vec(packet: Data) -> bool {
+        let mut file_manager = FileManager::default();
+        assert_eq!(None, file_manager.map.get(&packet.file_id));
+        file_manager.process_packet(Packet::Data(packet.clone()));
+
+        let group = file_manager.map.get(&packet.file_id).unwrap();
+        assert_eq!(None, group.file_name);
+        if packet.is_last_packet {
+            assert_eq!(Some((packet.packet_number as usize)+1), group.expected_number_of_packets);
+        } else {
+            assert_eq!(None, group.expected_number_of_packets);
+        }
+        assert_eq!(1, group.packets.len());
+        group.packets.get(&packet.packet_number).unwrap().deref() == packet.data
+    }
+}
+
+#[cfg(test)]
+mod all_packets_tests {
+    use rand::seq::SliceRandom;
+    use rand::thread_rng;
+
+    use crate::packets::{Header, Data, Packet};
+
+    use super::FileManager;
+
+    #[test]
+    fn processes_full_packet_set() {
+        let mut packets = Vec::new();
+        let file_name = "test_file_name".to_string();
+        let file_id = 42;
+        let num_packets: u16 = 3;
+        packets.push(Packet::Header(Header { file_name: file_name.clone(), file_id }));
+        for packet_number in 0..num_packets {
+            let val: u8 = (packet_number % 100).try_into().unwrap();
+            packets.push(Packet::Data(Data { file_id, packet_number, is_last_packet: packet_number == 2, 
+                data: vec![val, val+1]}));
+        }
+        let mut rng = thread_rng();
+
+        let mut file_manager = FileManager::default();
+        packets.shuffle(&mut rng);
+        for p in packets {
+            file_manager.process_packet(p);
+        }
+
+        assert_eq!(1, file_manager.map.len());
+        let group = file_manager.map.get(&file_id).unwrap();
+        assert_eq!(Some(file_name), group.file_name);
+        assert_eq!(Some(3), group.expected_number_of_packets);
+        for packet_number in 0..num_packets {
+            let data = group.packets.get(&packet_number).unwrap();
+            assert_eq!(2, data.len());
+            let val: u8 = (packet_number % 100).try_into().unwrap();
+            assert_eq!(val, data[0]);
+            assert_eq!(val+1, data[1]);
+        }
+    }
+}
