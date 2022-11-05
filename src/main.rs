4a3fed64c45013d53108c714bcbe83fc85faba90
Nic McPhee <mcphee@morris.umn.edu>
2022-11-05 16:34:37 -0500

    Add anyhow contexts to all the `?`s in `main`
    This adds contexts to all the various things that could panic in `main`, which actually helped me figure out where a connection error was coming from.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::io::{self, Write};
 
 use tokio::net::UdpSocket;
 
-use anyhow::Result;
+use anyhow::{Result, Context};
 
 use rust_segmented_file_client::{
     packets::{Packet, PacketParseError}, 
@@ -31,19 +31,34 @@ impl From<PacketParseError> for ClientError {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let sock = UdpSocket::bind("0.0.0.0:7077").await?;
+    let sock 
+        = UdpSocket::bind("0.0.0.0:7077")
+            .await
+            .context(format!("Failed to bind to 0.0.0.0:7077"))?;
 
     let remote_addr = "127.0.0.1:6014";
-    sock.connect(remote_addr).await?;
-    let mut buf = [0; 1028];
+    sock.connect(remote_addr)
+        .await
+        .context(format!("Failed to connect to remove address {remote_addr:?}"))?;
 
-    let _ = sock.send(&buf[..1028]).await?;
+    let mut buf = [0; 1028];
+    let _ 
+        = sock.send(&buf[..1028])
+            .await
+            .context(format!("Failed to send initial connect message"))?;
 
     let mut file_manager = FileManager::default();
 
     while !file_manager.received_all_packets() {
-        let len = sock.recv(&mut buf).await?;
-        let packet: Packet = buf[..len].try_into()?;
+        let len 
+            = sock.recv(&mut buf)
+                .await
+                .context(format!("Error receiving UDP packet"))?;
+        let packet: Packet
+            = buf[..len]
+                .try_into()
+                // TODO: Remove this context since it's not helpful?
+                .context(format!("Failed to convert data to Packet"))?;
         print!(".");
         io::stdout().flush()?;
         file_manager.process_packet(packet);
